38f3f037195ecc5db75da001055b5b1f8356bcce
AmazeingGames <106894328+AmazeingGames@users.noreply.github.com>
2026-03-31 17:09:39 -0700

    Fixed Null Reference
    Assigned UI manager in Bootstrap

--- Scripts/Bootstrap.gd
+++ Scripts/Bootstrap.gd
@@ -6,7 +6,6 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ui_manager.assign_player(player.find_child("HealthComponent"))
-
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
